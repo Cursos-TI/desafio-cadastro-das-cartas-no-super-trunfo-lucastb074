f5caa0ced727ed14b691c9329cc0be09add9724e
lucastb074 <47652630+lucastb074@users.noreply.github.com>
2025-03-10 20:08:21 +0000

    falta mt coisa

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,9 +15,10 @@ float PIBpercapita;
 
 };
 
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+// Função para calcular os valores derivados
+void calcularValores(struct Carta *carta) {
+    carta->densidadePopulacional = carta->populacao / carta->area;
+    carta->pibPerCapita = carta->pib / carta->populacao;
 
 int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
